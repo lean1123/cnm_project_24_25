4f09afcc5286e7feb785ffd980dfd34b4cae1a99
lean1123 <lethanhan20039@gmail.com>
2025-02-28 21:44:31 +0700

    [be/feature/call]: Enhance Call and Contact modules with Mongoose integration and throttling guard

--- g5-chatapp-be/src/call/call.controller.ts
+++ g5-chatapp-be/src/call/call.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('call')
-export class CallController {}
+export class CallController {
+  constructor() {}
+}
--- g5-chatapp-be/src/contact/contact.module.ts
+++ g5-chatapp-be/src/contact/contact.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Contact, ContactSchema } from './schema/contact.schema';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  providers: [ContactService],
-  controllers: [ContactController]
+  imports: [
+    MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
+  ],
+  providers: [
+    ContactService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+  controllers: [ContactController],
+  exports: [ContactService, MongooseModule],
 })
 export class ContactModule {}
