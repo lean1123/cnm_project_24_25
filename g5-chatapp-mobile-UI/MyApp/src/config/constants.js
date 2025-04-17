baa7bf156d484bb1b8161122c4e8a8c081047262
DucNhat <cuem8h@gmail.com>
2025-04-17 23:32:49 +0700

    [mobile/feature/chat]: handle send Video, Audio and fix UI

--- g5-chatapp-mobile-UI/MyApp/src/config/constants.js
+++ g5-chatapp-mobile-UI/MyApp/src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  "https://918b-2405-4800-5717-a460-153a-1ad2-c1e1-601c.ngrok-free.app";
+  "https://baad-2405-4800-5717-a460-153a-1ad2-c1e1-601c.ngrok-free.app";
 export const SOCKET_URL =
-  "wss://918b-2405-4800-5717-a460-153a-1ad2-c1e1-601c.ngrok-free.app";
+  "wss://baad-2405-4800-5717-a460-153a-1ad2-c1e1-601c.ngrok-free.app";
