9af869bef2dd45af23cefc1d98637b633a6cb2fd
DucNhat <cuem8h@gmail.com>
2025-04-19 23:04:47 +0700

    [mobile/fixV2/message]: update message

--- g5-chatapp-mobile-UI/MyApp/src/config/constants.js
+++ g5-chatapp-mobile-UI/MyApp/src/config/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://c38c-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app";  
-export const SOCKET_URL = "wss://c38c-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app"; 
+export const API_URL = "https://4801-2405-4800-5717-a460-1597-fd85-b66c-320.ngrok-free.app";  
+export const SOCKET_URL = "https://4801-2405-4800-5717-a460-1597-fd85-b66c-320.ngrok-free.app"; 
 
 
-export const PING_ENDPOINT = `${API_URL}/auth/check`; 
+// export const PING_ENDPOINT = `${API_URL}/auth/check`; 
