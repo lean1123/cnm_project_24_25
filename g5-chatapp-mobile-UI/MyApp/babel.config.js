23eaf57f1c1988af5700ac09ac22faee6c2feaed
DucNhat <cuem8h@gmail.com>
2025-04-20 10:38:26 +0700

    [mobile/fix/message]: babel config

--- g5-chatapp-mobile-UI/MyApp/babel.config.js
+++ g5-chatapp-mobile-UI/MyApp/babel.config.js
@@ -4,10 +4,6 @@ module.exports = {
     ["@babel/plugin-transform-class-properties", { loose: false }],
     ["@babel/plugin-transform-private-methods", { loose: false }],
     ["@babel/plugin-transform-private-property-in-object", { loose: false }],
-<<<<<<< HEAD
-    "react-native-reanimated/plugin",
-=======
     "react-native-reanimated/plugin" // LUÔN để cuối cùng theo hướng dẫn của Reanimated
->>>>>>> 366bb9ff7a776c3be9f712bf0806204cd635b047
   ],
 };
