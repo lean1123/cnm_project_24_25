703d78a4b246a50a5f177627bc8f01f4d9b1e75a
HoangHuy-23 <hh.glorious@gmail.com>
2025-05-24 17:38:59 +0700

    clear old source

--- g5-chatapp-fe/app/(root)/contacts/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const ContactsPage = (props: Props) => {
-  return <div>ContactsPage</div>;
-};
-
-export default ContactsPage;
--- g5-chatapp-fe/lib/format.ts
+++
@@ -1,9 +0,0 @@
-
-export const toStringFromDate = (date: Date) => {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
-}
