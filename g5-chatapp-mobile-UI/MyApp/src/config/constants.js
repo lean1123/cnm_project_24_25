b1c790bca443abbf5abb8939d7f6aa7e7240b37e
lean1123 <lethanhan20039@gmail.com>
2025-05-24 20:22:00 +0700

    commit before merge

--- g5-chatapp-mobile-UI/MyApp/src/config/constants.js
+++ g5-chatapp-mobile-UI/MyApp/src/config/constants.js
@@ -1,8 +1,10 @@
 // export const API_URL = "https://1be5-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app";
 // export const SOCKET_URL = "wss://1be5-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app";
 
-export const API_URL = "http://192.168.1.6:3000";
-export const SOCKET_URL = "http://192.168.1.6:3000";
+export const API_URL =
+  "https://450d-2402-800-63f9-3105-602f-48e9-547-6005.ngrok-free.app";
+export const SOCKET_URL =
+  "https://450d-2402-800-63f9-3105-602f-48e9-547-6005.ngrok-free.app";
 
 // export const API_URL = "https://3811-58-186-28-253.ngrok-free.app";
 // export const SOCKET_URL = "wss://3811-58-186-28-253.ngrok-free.app";
