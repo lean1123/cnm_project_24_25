53bc8f3553f04f8a6733463d1b942a1430734fea
lean1123 <lethanhan20039@gmail.com>
2025-05-25 16:26:17 +0700

    handle when not found static files, fallback index.html to get resource

--- g5-chatapp-mobile-UI/MyApp/src/config/constants.js
+++ g5-chatapp-mobile-UI/MyApp/src/config/constants.js
@@ -1,10 +1,2 @@
-// export const API_URL = "https://1be5-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app";
-// export const SOCKET_URL = "wss://1be5-2405-4800-5717-a460-13b-a83f-d139-8b6.ngrok-free.app";
-
-export const API_URL =
-  "https://cef6-2402-800-63f9-3105-602f-48e9-547-6005.ngrok-free.app";
-export const SOCKET_URL =
-  "https://cef6-2402-800-63f9-3105-602f-48e9-547-6005.ngrok-free.app";
-
-// export const API_URL = "https://3811-58-186-28-253.ngrok-free.app";
-// export const SOCKET_URL = "wss://3811-58-186-28-253.ngrok-free.app";
+export const API_URL = "https://cnm-project-24-25.onrender.com";
+export const SOCKET_URL = "https://cnm-project-24-25.onrender.com";
